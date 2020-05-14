6e0d11991f081a383caf55e4c883b9332e120768
sutej-truetechpro <sutej@truetechpro.com>
2020-05-14 15:29:49 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import '@/assets/css/tailwind.css'
+import Toasted from 'vue-toasted';
+
+Vue.use(Toasted,
+  {position: "top-right", duration: 1500, keepOnHover: true}
+);
 
 Vue.config.productionTip = false
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/login/login.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
   {
     path: '/',
     name: 'Home',
